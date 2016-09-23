ea61a5b0f699b9470f57fb33051503f5469731b4
helboy13337 <conanm.ps3@gmail.com>
2016-09-23 13:46:40 +0200

    adde tables and massagbox

--- Calculix/Calculix/Form1.cs
+++ Calculix/Calculix/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Calculix
 {
     public partial class Form1 : Form
@@ -19,6 +20,20 @@ namespace Calculix
 
         private void button1_Click(object sender, EventArgs e)
         {
+            DataTable dt = new DataTable();
+            string leftside = textBox1.Text.Split('=')[0];
+            string rightside = textBox1.Text.Split('=')[1];
+            var result1 = dt.Compute(leftside, "");
+            var result2 = dt.Compute(rightside, "");
+            if (Convert.ToDouble(result1) == Convert.ToDouble(result2))
+            {
+                //MessageBox.Show(result1.ToString()+" "+ result2.ToString());
+                MessageBox.Show("this is correct");
+            }
+            else
+            {
+                MessageBox.Show("this is false");
+            }
 
         }
     }
